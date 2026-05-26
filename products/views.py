830f7d0b57f3c4d4d119228b4fc34ec1a0bd8f71
umut can karali <umutcankarali53@gmail.com>
2026-05-26 23:18:22 +0300

    Yapay zeka import kodu guncellendi

--- products/views.py
+++ products/views.py
@@ -5,12 +5,11 @@ from .forms import ReviewForm
 import stripe
 from django.conf import settings
 from django.urls import reverse
-import google.generativeai as genai
 from django.conf import settings
 from django.http import JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
-
+from google import genai
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
